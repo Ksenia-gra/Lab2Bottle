998d0699c6ae9cb63af0f51a7116063ecd5b259b
Ксения Графова <kseniiagrafova@icloud.com>
2023-04-15 01:33:42 +0300

    Работа с Json и добавление проверок

--- Lab2Bottle/myform.py
+++ Lab2Bottle/myform.py
@@ -2,7 +2,7 @@ from bottle import post, request,view,template,datetime
 import re
 import pdb
 import json
-from os.path import exists
+import os
 from datetime import date
 
 @post('/myform',method='post')
@@ -12,23 +12,33 @@ def my_form():
     name = request.forms.get('NAME')
     question = request.forms.get('QUEST')
     current_date = date.today()
-    regex=re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
     match = re.match(regex, mail)
-    if(question=="" or question==" "):
+    if(question == "" or question == " "):
         return template('index.tpl',year=datetime.now().year,error="Fill in field Your Question",quest=question,name=name,email=mail)
-    elif (name=="" or name==" "):
+    elif(len(question)<=3):
+        return template('index.tpl',year=datetime.now().year,error="Your Question must be more than 3 symbols",quest=question,name=name,email=mail)
+    elif(question.isdigit()):
+        return template('index.tpl',year=datetime.now().year,error="Your Question have to contains not only digits!",quest=question,name=name,email=mail)
+    elif (name == "" or name == " "):
         return template('index.tpl',year=datetime.now().year,error="Fill in field Your Name",quest=question,name=name,email=mail)
-    elif(mail=="" or mail==" "):
+    elif(mail == "" or mail == " "):
         return template('index.tpl',year=datetime.now().year,error="Fill in field Your Email",quest=question,name=name,email=mail)
     elif not match:
         return template('index.tpl',year=datetime.now().year,error="Email isn't match the format",quest=question,name=name,email=mail) 
     else:
         questions={}
-        if mail in questions:
-            questions.get(mail).append(question)
-        else:
-            questions={mail:[name]}
-            questions.get(mail).append(question)
-        pdb.set_trace()
+        if os.path.exists('user_questions.json'):
+            with open('user_questions.json', 'r') as read_json:
+                if (os.path.getsize('user_questions.json') > 0):
+                    questions = json.load(read_json)
+            with open('user_questions.json', 'w') as write_json:
+                if mail in questions:
+                    if question.lower() not in questions.get(mail):
+                        questions.get(mail).append(question.lower())
+                else:
+                    questions[mail]=[name]
+                    questions.get(mail).append(question.lower())
+                json.dump(questions, write_json)
         return dict(message="Thanks, {0} !The answer will be sent to the mail {1}. Access date: {2} ".format(name,mail,current_date))
         
