867833d8144ac39b5432a4221329b720c0cf4462
Ксения Графова <kseniiagrafova@icloud.com>
2023-04-15 00:19:41 +0300

    Изменение структуры словаря

--- Lab2Bottle/myform.py
+++ Lab2Bottle/myform.py
@@ -1,6 +1,8 @@
 from bottle import post, request,view,template,datetime
 import re
 import pdb
+import json
+from os.path import exists
 from datetime import date
 
 @post('/myform',method='post')
@@ -18,11 +20,15 @@ def my_form():
         return template('index.tpl',year=datetime.now().year,error="Fill in field Your Name",quest=question,name=name,email=mail)
     elif(mail=="" or mail==" "):
         return template('index.tpl',year=datetime.now().year,error="Fill in field Your Email",quest=question,name=name,email=mail)
-    elif match:
-        questions={mail:question}
+    elif not match:
+        return template('index.tpl',year=datetime.now().year,error="Email isn't match the format",quest=question,name=name,email=mail) 
+    else:
+        questions={}
+        if mail in questions:
+            questions.get(mail).append(question)
+        else:
+            questions={mail:[name]}
+            questions.get(mail).append(question)
         pdb.set_trace()
         return dict(message="Thanks, {0} !The answer will be sent to the mail {1}. Access date: {2} ".format(name,mail,current_date))
-    else:
-        return template('index.tpl',year=datetime.now().year,error="Email isn't match the format",quest=question,name=name,email=mail) 
-    
-
+        
